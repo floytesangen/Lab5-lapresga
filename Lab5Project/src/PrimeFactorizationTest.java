812e3f206febf7339ba5f809eba77afa0d3f4c50
Greg <lapresga@rose-hulman.edu>
2011-03-24 15:18:24 -0400

    wrote a basic test to test junit

---
+++ Lab5Project/src/PrimeFactorizationTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class PrimeFactorizationTest {
+	@Test
+	public void testJUnit()
+	{
+		assertTrue("Hello", true);
+	}
+	
+	
+}
