86f01cb594f50b0a4ac600aec9effb3244aa794e
Greg <lapresga@rose-hulman.edu>
2011-03-28 21:34:16 -0400

    lapresga passed testTwo

--- Lab5Project/src/PrimeFactorization.java
+++ Lab5Project/src/PrimeFactorization.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 
 public class PrimeFactorization {
 	public static ArrayList<Integer> generatePrimes(int i){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> ret;
+	
+		ret = new ArrayList<Integer>();
+		
+		if (i > 1)
+		{
+			ret.add(2);
+		}
+		return ret;
 	}
 }
