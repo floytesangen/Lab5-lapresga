8c8c3c57fa2867f59c95f9d9f0878743741a47fd
Greg <lapresga@rose-hulman.edu>
2011-03-28 22:50:10 -0400

    lapresga passed testLarge

--- Lab5Project/src/PrimeGeneration.java
+++ Lab5Project/src/PrimeGeneration.java
@@ -5,20 +5,12 @@ public class PrimeGeneration {
 
 	public static Object generatePrimes(int i) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		if (i > 2) ret.add(2);
+		
 		for (int candidate = 2; candidate < i; candidate++)
 		{
-
-			
-			for (int n = 2; n < candidate; n++)
+			if (PrimeFactorization.generatePrimes(candidate).size() == 1)
 			{
-
-				if(candidate % n == 0){
-					continue;
-				}
-				else{
-					ret.add(candidate);
-				}
+				ret.add(candidate);
 			}
 		}
 		return ret;
