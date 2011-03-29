24d7915a003d5462a3e8b3797f91fc764f90e93f
Greg <lapresga@rose-hulman.edu>
2011-03-28 22:17:09 -0400

    siegleal passed testFour

--- Lab5Project/src/PrimeGeneration.java
+++ Lab5Project/src/PrimeGeneration.java
@@ -5,9 +5,21 @@ public class PrimeGeneration {
 
 	public static Object generatePrimes(int i) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		if (i > 2)
+		if (i > 2) ret.add(2);
+		for (int candidate = 2; candidate < i; candidate++)
 		{
-			ret.add(2);
+
+			
+			for (int n = 2; n < candidate; n++)
+			{
+
+				if(candidate % n == 0){
+					continue;
+				}
+				else{
+					ret.add(candidate);
+				}
+			}
 		}
 		return ret;
 	}
